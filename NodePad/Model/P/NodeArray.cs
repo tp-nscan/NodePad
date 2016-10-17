7b7a02b8534edf9382d4df3603cd87cce1f0b515
tp-nscan <tpatters@pc.cognex.com>
2016-10-17 16:27:49 -0400

    UpdateStar

---
+++ NodePad/Model/P/NodeArray.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodePad.Model.P
+{
+    public class NodeArray
+    {
+        public NodeArray(double[] initVal)
+        {
+            _arrayA = initVal;
+            _arrayB = new double[_arrayA.Length];
+            ArraySize = _arrayA.Length;
+            ArrayStride = (int) Math.Sqrt(ArraySize);
+            _generation = 0;
+        }
+
+        double[] _arrayA;
+        double[] _arrayB;
+        public int ArraySize { get; }
+        public int ArrayStride { get; }
+
+        private int _generation;
+        public int Generation => _generation;
+
+        public IReadOnlyList<double> Current => 
+            (Generation % 2 == 0) ? _arrayA.ToList() : _arrayB.ToList();
+
+        double[] InputArray
+        {
+            get { return (Generation%2 == 0) ? _arrayA : _arrayB; }
+            set
+            {
+                if (Generation % 2 == 0)
+                {
+                    _arrayA = value;
+                }
+                else
+                {
+                    _arrayB = value;
+                }
+            }
+        }
+
+
+        double[] OutputArray
+        {
+            get { return (Generation%2 == 0) ? _arrayB : _arrayA; }
+            set
+            {
+                if (Generation % 2 == 0)
+                {
+                    _arrayB = value;
+                }
+                else
+                {
+                    _arrayA = value;
+                }
+            }
+        }
+
+        void ProcStretch2d(int curChunk, int chunkCount, double[] input, double[] output,  double cpl, double noiseLevel, Random randy)
+        {
+            var alength = output.Length;
+            var slength = (int)Math.Sqrt(alength);
+            var start = curChunk * alength / chunkCount;
+            var end = curChunk * alength / chunkCount + alength / chunkCount;
+            var halfCpl = cpl / 2;
+
+            for (var i = start; i < end; i++)
+            {
+                var nbrs = input[(i + 1 + alength) % alength]
+                         + input[(i - 1 + alength) % alength]
+                         + input[(i - slength + alength) % alength]
+                         + input[(i + slength + alength) % alength];
+
+                var res = cpl * nbrs + input[i] + randy.NextDouble() * noiseLevel - halfCpl;
+                output[i] = (res < -1.0) ? -1.0 : (res > 1.0) ? 1.0 : res;
+            }
+        }
+
+        public static NodeArray UpdateStar(NodeArray nodeArray, double cpl, double noise, Random randy)
+        {
+            var input = nodeArray.InputArray;
+            var output = nodeArray.OutputArray;
+            nodeArray._generation++;
+
+            var chunkCount = 8;
+
+            Parallel.For((long) 0, chunkCount, i => nodeArray.ProcStretch2d(
+                curChunk: (int)i, 
+                chunkCount: chunkCount,
+                input: input,
+                output: output, 
+                cpl: cpl,
+                noiseLevel: noise,
+                randy: randy));
+
+            nodeArray.InputArray = output;
+            return nodeArray;
+        }
+    }
+}
--- NotePad.Test/ParallelTest.cs
+++ NotePad.Test/ParallelTest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Security.AccessControl;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NodePad.Common;
+using NodePad.Model.P;
 
 namespace NodePad.Test
 {
@@ -93,10 +95,8 @@ namespace NodePad.Test
         [TestMethod]
         public void Parallel2d()
         {
-            var alength = 262144;
-            var cpl = 0.1;
-            var noise = 0.1;
-
+            var alength = 16384;
+            var cpl = 0.001;
             var rando = new ThreadSafeRandom();
             var ape1 = new double[alength];
             var ape2 = new double[alength];
@@ -109,13 +109,13 @@ namespace NodePad.Test
                 ape1[i] = randy.NextDouble() * 2 - 1.0;
             }
 
-            for (var k = 0; k < 1; k++)
+            for (var k = 0; k < 8; k++)
             {
                 sw.Start();
                 var chunks = 4;
                 for (var s = 0; s < 40; s++)
                 {
-                    noise = 0.1 - 0.002 * s;
+                    var noise = 0.1 - 0.002 * s;
                     for (var q = 0; q < 50; q++)
                     {
                         var noise1 = noise;
@@ -124,11 +124,6 @@ namespace NodePad.Test
                     }
                     Debug.WriteLine("{0}\t{1}\t{2}", s, noise, Correlo(ape1));
                 }
-            //for (var s = 0; s < 1000; s++)
-                //{
-                //    ProcStretch2d(0, 1, ape1, ape2, cpl, noise, rando);
-                //    ProcStretch2d(0, 1, ape2, ape1, cpl, noise, rando);
-                //}
 
                 sw.Stop();
                 Debug.WriteLine("Elapsed={0}", sw.Elapsed);
@@ -136,9 +131,47 @@ namespace NodePad.Test
             }
         }
 
-        void ProcStretch2d(int chunk, int max, double[] aRay, double[] bRay, double cpl, double noiseLevel, Random randy)
+        [TestMethod]
+        public void ParallelArray2d()
         {
-            var alength = aRay.Length;
+            var alength = 16384;
+            var cpl = 0.1;
+
+            var rando = new ThreadSafeRandom();
+
+            var sw = new Stopwatch();
+            var randy = new Random();
+
+            var nodeArray = new NodeArray(
+                Enumerable.Range(0, alength)
+                          .Select(i=> randy.NextDouble() * 2 - 1.0)
+                          .ToArray());
+
+            for (var k = 0; k < 8; k++)
+            {
+                sw.Start();
+                for (var s = 0; s < 40; s++)
+                {
+                    var noise = 0.1 - 0.002 * s;
+                    for (var q = 0; q < 50; q++)
+                    {
+                        nodeArray = NodeArray.UpdateStar(nodeArray, cpl, noise, randy);
+                        nodeArray = NodeArray.UpdateStar(nodeArray, cpl, noise, randy);
+                    }
+                    Debug.WriteLine("{0}\t{1}\t{2}", s, noise, Correlo(nodeArray.Current.ToArray()));
+                }
+
+                sw.Stop();
+                Debug.WriteLine("Elapsed={0}", sw.Elapsed);
+                sw.Reset();
+            }
+        }
+
+
+
+        void ProcStretch2d(int chunk, int max, double[] output, double[] input, double cpl, double noiseLevel, Random randy)
+        {
+            var alength = output.Length;
             var slength = (int)Math.Sqrt(alength);
             var start = chunk * alength / max;
             var end = chunk * alength / max + alength / max;
@@ -146,13 +179,13 @@ namespace NodePad.Test
 
             for (var i = start; i < end; i++)
             {
-                var nbrs = bRay[(i + 1 + alength) % alength] 
-                         + bRay[(i - 1 + alength) % alength]
-                         + bRay[(i - slength + alength) % alength]
-                         + bRay[(i + slength + alength) % alength];
+                var nbrs = input[(i + 1 + alength) % alength] 
+                         + input[(i - 1 + alength) % alength]
+                         + input[(i - slength + alength) % alength]
+                         + input[(i + slength + alength) % alength];
 
-                var res = cpl * nbrs + bRay[i] + randy.NextDouble() * noiseLevel - halfCpl;
-                aRay[i] = (res < -1.0) ? -1.0 : (res > 1.0) ? 1.0 : res;
+                var res = cpl * nbrs + input[i] + randy.NextDouble() * noiseLevel - halfCpl;
+                output[i] = (res < -1.0) ? -1.0 : (res > 1.0) ? 1.0 : res;
             }
         }
 
